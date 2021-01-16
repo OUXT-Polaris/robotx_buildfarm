bc260861fdd6a7845438cf7711f952eb7dcda5d2
Masaya Kataoka <ms.kataoka@gmail.com>
2021-01-16 14:55:47 +0900

    update index
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- report.py
+++ report.py
@@ -6,8 +6,9 @@ import datetime
 
 def report(token, yaml_path):
     f = open('docs/report.md', 'w')
-    f.write("# Reports Generated " + str(datetime.datetime(2017, 11, 12, 9, 55, 28)) + "  \n")
-    f.write("## Issues  \n")
+    f.write("# Reports  \n")
+    f.write("last update " + str(datetime.datetime(2017, 11, 12, 9, 55, 28)) + "  \n")
+    f.write("## Issues/Pull Requests  \n")
     f.write("### ROS2 packages  \n")
     f.write(pd.DataFrame(get_ros_package_issues(token, yaml_path), columns=['package','issue']).to_markdown())
     f.close()
