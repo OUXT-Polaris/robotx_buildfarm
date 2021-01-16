65b2a2a3b5ae0ba83ee3ce59bc5d0cf42b426377
Masaya Kataoka <ms.kataoka@gmail.com>
2021-01-16 20:36:21 +0900

    enable clone repos from yaml
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- clone_ros_packages.py
+++ clone_ros_packages.py
@@ -2,17 +2,19 @@ import argparse
 from github import Github
 import yaml
 import git
+import os
 
 def clone_ros_packages(token, yaml_path):
-    repo_path = os.path.join('./', 'ros_packages')
     g = Github(token)
     with open(yaml_path) as file:
         config = yaml.safe_load(file)
         for user in config["ros"]:
             for package in config["ros"][user]:
+                repo_path = os.path.join('./', 'ros_packages/' + package)
                 url = "https://github.com/" + user + "/" + package + ".git"
                 print("scanning -> " + url)
                 repo = g.get_repo(user + "/" + package)
+                git_repo = git.Repo.clone_from(repo.clone_url, repo_path, branch=repo.default_branch)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='scripts for getting issues')
